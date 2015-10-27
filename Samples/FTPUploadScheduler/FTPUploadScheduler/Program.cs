f6be6c62543dd9e6784fae1babd93f396075bab4
saravanaesakki <saravananesakki@gmail.com>
2015-10-27 15:41:18 +0530

    dotnetsamples

---
+++ Samples/FTPUploadScheduler/FTPUploadScheduler/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FTPUploadScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
