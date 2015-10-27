1130b1fc1f5246fb7be2274f1ed155deb54bf2d0
saravanaesakki <saravananesakki@gmail.com>
2015-10-27 15:50:55 +0530

    dotnetsamples

---
+++ Samples/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,12 @@
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim a As Integer = "999999999 ".ToString.Trim.PadLeft(9, "0")
+
+
+
+
+    End Sub
+End Class
+
+
