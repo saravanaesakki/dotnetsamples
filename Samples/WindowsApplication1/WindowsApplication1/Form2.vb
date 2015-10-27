82e1453bdb23d2d6130d9d3e1ae1d7dc586386f6
saravanaesakki <saravananesakki@gmail.com>
2015-10-27 15:50:59 +0530

    dotnetsamples

---
+++ Samples/WindowsApplication1/WindowsApplication1/Form2.vb
@@ -0,0 +1,34 @@
+Public Class Form2
+    'Dim obj As New Greg.XmlEditor.Presentation.Views.XmlTextEditor
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        '  Panel2.Controls.Add(NavigationPane1)
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ' Dim XMLValid As New Greg.XmlEditor.Business.Concrete.XmlValidator
+        'XMLValid.Validate(obj.Text)
+        'MsgBox(XMLValid.ValidationResult)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Dim obj As New Ascend.Border
+        'Dim obj1 As New Ascend.Windows.Forms.GradientCaption
+        'obj1.Dock = DockStyle.Fill
+        'obj1.Text = "Welcome"
+        'obj1.WatermarkImage = Image.FromFile("c:\1.jpg")
+        'Panel2.Controls.Add(obj1)
+
+        '   obj.Dock = DockStyle.Fill
+        '  Panel2.Controls.Add(obj)
+    End Sub
+
+    
+    Private Sub NavigationPanePage1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NavigationPanePage1.Click
+
+    End Sub
+
+    Private Sub NavigationPanePage2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NavigationPanePage2.Click
+
+    End Sub
+End Class
